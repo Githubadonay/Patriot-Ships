42255224033c398c93f3f33732b18f756022c448
adonayaraia02@gmail.com <adonayaraia02@gmail.com>
2025-03-01 14:46:11 -0500

    Patriot Ships is a new cruise line company which has a fleet of 10 cruise ships, each with a capacity of 300 passengers. To manage its opera=ons eﬃciently, the company is looking for a program that can help track its fleet, manage bookings, and calculate revenue for each cruise.

--- src/cruise.java
+++ src/cruise.java
@@ -2,53 +2,84 @@ public class cruise {
     private String cruiseID;
     private String route;
     private double ticketPrice;
-    public static int maxNumOfCruise = 10;
+    private int ticketSold;
+    public static int currentNumOfPassengers;
 
-   // public static int
+    // public static int
 
-cruise(){
-    this.cruiseID = ""; 
-    this.route = ""; 
-    this.ticketPrice = 0; 
-};
-cruise(String cruiseID, String route, double ticketPrice){
-    this.cruiseID = cruiseID;
-    this.route = route;
-    this.ticketPrice = ticketPrice;
-}
+    cruise() {
+        this.cruiseID = "";
+        this.route = "";
+        this.ticketPrice = 0;
+        this.ticketSold = 0;
+        currentNumOfPassengers++;
+    };
 
-// get
-public String getCuiseID(){
-    return this.cruiseID;
-}
-public String getRoute(){
-    return this.route;
-}
-public double getTicketPrice(){
-    return this.ticketPrice;
-}
-
-//set
-public void setCruiseID(String cruiseID){
-    if(!cruiseID.equals(""))
-    this.cruiseID = cruiseID;
-}
-
-public void setRoute(String route){
-    if(!route.equals(""))
-    this.route = route;
-}
-
-public void setTicketPrice(double ticketPrice){
-    if(ticketPrice > 0){
+    cruise(String cruiseID, String route, double ticketPrice, int ticketSold) {
+        this.cruiseID = cruiseID;
+        this.route = route;
         this.ticketPrice = ticketPrice;
+        this.ticketSold = ticketSold;
+        currentNumOfPassengers++;
     }
+
+    // get
+    public String getCuiseID() {
+        return this.cruiseID;
+    }
+
+    public String getRoute() {
+        return this.route;
+    }
+
+    public double getTicketPrice() {
+        return this.ticketPrice;
+    }
+
+    public int getTicketSold() {
+        return ticketSold;
+    }
+
+    public static int getCurrentNumOfPassengers() {
+        return currentNumOfPassengers;
+    }
+
+    // set
+    public void setCruiseID(String cruiseID) {
+        if (!cruiseID.equals(""))
+            this.cruiseID = cruiseID;
+    }
+
+    public void setRoute(String route) {
+        if (!route.equals(""))
+            this.route = route;
+    }
+
+    public void setTicketPrice(double ticketPrice) {
+        if (ticketPrice > 0) {
+            this.ticketPrice = ticketPrice;
+        }
+    }
+
+    public void setTicketSold(int ticketSold) {
+        if (ticketSold > 0) {
+            this.ticketSold = ticketSold;
+        }
+    }
+
+    public void setCurrentNumOfPassengers(int currentNumOfPassengers) {
+        currentNumOfPassengers--;
+    }
+
+    // calculation for the earing of ticker sold. ticketCost * ticketSold
+    public double totalEarning() {
+        return ticketPrice * ticketSold;
+    }
+
+    // toString
+    public String toString() {
+        return "Cruise ID | Cruise Route | Ticket Cost | Ticket sold | Total Earning\n"
+                + getCuiseID() + getRoute() + getTicketPrice() + getTicketSold() + totalEarning() + "\n";
+    };
+
 }
-
-// calculation 
-
-//toString
-
-}
-
-
--- src/curiseImp.java
+++ src/curiseImp.java
@@ -1,3 +1,43 @@
+import javax.swing.JOptionPane;
+
 public class curiseImp {
+    public static void main(String[] args) {
+        final int maxNumOfPassenger = 300;
+        final int maxNumOfCruise = 10;
+        // public static int NumOfCruise = 10;
+
+        cruise[] allCruise = new cruise[maxNumOfCruise];// setting the array for cruise
+        int choice = 0;
+        // menu using do-while and switch case
+        do {
+            choice = Integer.parseInt(JOptionPane.showInputDialog(
+                    "Choose one of the following option: \n1. Create Cruise \n2. Search Cruise \n3. Remove Cruise \n4. Sell Cruise Ticket \n5. Display All Cruise \n6. Exit \n Enter your choice"));
+            switch (choice) {
+                case 1:
+
+                    break;
+                case 2:
+
+                    break;
+                case 3:
+
+                    break;
+                case 4:
+
+                    break;
+                case 5:
+
+                    break;
+                case 6:
+
+                    break;
+
+                default:
+                    break;
+            }
+        } while (choice >= 1 && choice <= 6);
+        JOptionPane.showMessageDialog(null, choice);
+    }
+
     
 }
